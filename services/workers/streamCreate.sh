12942d81e4fb58aa596781f508ee1e20b9270650
tripoloskies <rodel.rizalde09@gmail.com>
2026-03-22 04:44:25 +0000

    Refactor streamCreate.sh and added optional logging for downloading progress.

--- services/workers/streamCreate.sh
+++ services/workers/streamCreate.sh
@@ -1,10 +1,7 @@
-
 #!/bin/bash
 
-
 # Environment Path Variables
-CURRENT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-PW_DIR=$(pwd)
+WORK_DIR=$(pwd)
 
 # Network/Hostname Variables
 HOST="localhost"
@@ -18,6 +15,7 @@ RUNTIME_PATH="$4"
 YTDLP_PATH="$5"
 STREAMLINK_PATH="$6"
 MEDIAMTX_HOST="$7"
+LOG_DL_PROGRESS="$8"
 
 # Background PID's to close in case of nasty moments while this script was running.
 PUBLISHER_PID=""
@@ -76,30 +74,45 @@ if [[ -z "$MEDIAMTX_HOST" ]]; then
     exit 1
 fi
 
+if [[ -z "$LOG_DL_PROGRESS" ]]; then
+    LOG_DL_PROGRESS="0"
+elif [[ "$LOG_DL_PROGRESS" != "1" && "$LOG_DL_PROGRESS" != "0" ]]; then
+    echo "Invalid downloading progress logging value. Value must be 1 (enabled) or 0 (disabled)"
+    exit 1
+fi
+
+getytdlpCookieArgs() {
+    if [[ "$USE_COOKIES" == "1" ]]; then
+        echo "--js-runtimes bun:$RUNTIME_PATH --cookies "$WORK_DIR/config/cookies.txt""
+    else
+        echo ""
+    fi
+}
+
 getVodId() {
-    echo $(curl -s -X POST http://$HOST:3002/publish -F "id=$STREAM_PATH" 2>&1)
+    curl -s -X POST http://$HOST:3002/publish -F "id=$STREAM_PATH" 2>&1
 }
 
 
 checkStreamIfBroken() {
-    echo $(curl -s -X POST http://$HOST:3002/verify -F "vodId=$VOD_ID" 2>&1)
+    curl -s -X POST http://$HOST:3002/verify -F "vodId=$VOD_ID" 2>&1
 }
 
 parseStreamMetadata() {
+    local METADATA
+    local SAVE_STATUS
+
     echo "Metadata extraction starting..."
 
-    if [[ "$USE_COOKIES" == 1 ]]; then
-        COOKIE_ARGS="--js-runtimes bun:$RUNTIME_PATH --cookies "$PW_DIR/config/cookies.txt""
-    else
-        COOKIE_ARGS=""
-    fi
+    # shellcheck disable=SC2046
+    METADATA=$($YTDLP_PATH $(getytdlpCookieArgs) -O "%(.{id,fulltitle,uploader,timestamp,description,extractor,webpage_url})#j" --no-warnings --skip-download "$SOURCE_URL" 1>&1 | base64)    
     
-    local METADATA=$($YTDLP_PATH $COOKIE_ARGS -O "%(.{id,fulltitle,uploader,timestamp,description,extractor,webpage_url})#j" --no-warnings --skip-download $SOURCE_URL 1>&1 | base64)
     if [[ "$METADATA" == "" ]]; then
         echo "Metadata extraction failed."
     else
         echo "Metadata successfully extracted."
-        local SAVE_STATUS=$(curl -s -X POST http://$HOST:3002/metadata -F "vodId=$VOD_ID" -F "metadata=$METADATA" 1>&1)
+
+        SAVE_STATUS=$(curl -s -X POST http://$HOST:3002/metadata -F "vodId=$VOD_ID" -F "metadata=$METADATA" 1>&1)
         if [[ "$SAVE_STATUS" == "0" ]]; then
             echo "Metadata saved successfully."
         else
@@ -107,14 +120,35 @@ parseStreamMetadata() {
         fi
     fi
 }
+
+
+getStreamTags() {
+    local URL=$1
+    local CODEC_LISTS
+    CODEC_LISTS=$($STREAMLINK_PATH --loglevel none --stdout "$URL" best 2> /dev/null | ffprobe -loglevel quiet -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 -analyzeduration 2k -probesize 2k -i -)
+
+    if [[ "$CODEC_LISTS" == *"h264"* ]]; then
+        echo "avc1"
+    elif [[ "$CODEC_LISTS" == *"h265"* ]]; then
+        echo "hvc1"
+    else
+        return 1
+    fi
+
+    if [[ "$CODEC_LISTS" == *"aac"* || "$CODEC_LISTS" == *"mp3"* || "$CODEC_LISTS" == *"opus"* || "$CODEC_LISTS" == *"ac3"* ]]; then
+        return 0
+    else
+        return 2
+    fi
+}
+
 publish() {
     local URL=$1
     local BUFFER_SIZE=$2
     local ARGS=$3
     local METADATA=$4
-
+    
     VOD_ID=$(getVodId)
-
 
     if [[ "$URL" == "" ]]; then
         inform_update "Broken Stream"
@@ -142,42 +176,57 @@ publish() {
 
     TMPDIR=$(mktemp -d /tmp/buffer.XXXXXX)
 
-    mkfifo $TMPDIR/filter1
-
     local A_DIR="$RECORDING_PATH/$STREAM_PATH/$VOD_ID"
+
+    mkfifo "$TMPDIR/filter1"
     mkdir -p "$A_DIR"
     mkdir -p "$A_DIR/segments"
 
-    $STREAMLINK_PATH --http-cookies-file "$PW_DIR/config/cookies.txt" --logfile "$A_DIR/logs.txt" --loglevel "all" --stream-segment-threads 3 $ARGS --ringbuffer-size 64M --stdout "$URL" best | \
-    mbuffer -q -m $BUFFER_SIZE -P 60 > $TMPDIR/filter1 &
-    MBUFFER_PID=$!
-
-    local VCODEC=$($STREAMLINK_PATH --stdout "$URL" best | ffprobe -loglevel quiet -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 -analyzeduration 2k -probesize 2k -i - | head -n 1)
-
-    if [[ "$VCODEC" == "h264" ]]; then
-        TAG="avc1"
-    elif [[ "$VCODEC" == "h265" ]]; then
-        TAG="hvc1"
+    if [[ $LOG_DL_PROGRESS == "1" ]]; then
+        echo "Downloading progress logging enabled."
+        local LOG_ARGS="--logfile "$A_DIR/logs.txt" --loglevel all"
     else
-        TAG="$VCODEC"
+        echo "Downloading progress logging disabled."
+        local LOG_ARGS=""
     fi
 
+    $STREAMLINK_PATH --http-cookies-file "$WORK_DIR/config/cookies.txt" $LOG_ARGS --stream-segment-threads 2 $ARGS --ringbuffer-size 64M --stdout "$URL" best | \
+    mbuffer -q -m "$BUFFER_SIZE" -P 60 > "$TMPDIR/filter1" &
+    
+    MBUFFER_PID=$!
 
-    ffmpeg -stats \
+    TAG=$(getStreamTags "$URL")
+    local CODEC_STATUS=$?
+    
+    if [[ $CODEC_STATUS == 0 ]]; then
+        local FFMPEG_MAP="-map 0:v -map 0:a"
+    elif [[ $CODEC_STATUS == 1 ]]; then
+        inform_status "No Video"
+        echo "Error: No Video. Exiting..."
+        exit 1
+    elif [[ $CODEC_STATUS == 2 ]]; then
+        echo "Warning: Unknown or no Audio Codec. Disabling audio..."
+        local FFMPEG_MAP="-map 0:v"
+    else
+       echo "Unknown Error: Exiting..."
+    fi
+
+    ffmpeg -hide_banner -stats \
     -thread_queue_size 8192 -fflags +genpts -re \
-    -i $TMPDIR/filter1 \
+    -i "$TMPDIR/filter1" \
     -c:v copy \
+    -flags +global_header \
+    $FFMPEG_MAP \
+    -af "aresample=async=1:first_pts=0" \
     -c:a aac \
     -b:a 192k \
-    -af "aresample=async=1:first_pts=0" -profile:a aac_low \
+    -profile:a aac_low \
     -aac_coder fast \
-    -flags +global_header \
-    -map 0:v -map 0:a \
     -aac_ms false \
     -aac_is false \
     -aac_pns false \
-    -tag:v "$TAG" \
     -aac_tns false \
+    -tag:v "$TAG" \
     -sn -dn \
     -avoid_negative_ts make_zero \
     -muxdelay 0.5 \
@@ -193,7 +242,7 @@ publish() {
             echo "The publisher was dead. Skipping..."
             break
         fi
-        ffmpeg -loglevel quiet -i $A_DIR/segments/segment1.ts -frames:v 1 -update true -y $A_DIR/thumbnail.jpg
+        ffmpeg -loglevel quiet -i "$A_DIR/segments/segment1.ts" -frames:v 1 -update true -y "$A_DIR/thumbnail.jpg"
         if [ -f "$A_DIR/thumbnail.jpg" ]; then
             echo "Thumbnail successfully created."
             if [[ "$METADATA" == "yes" ]]; then
@@ -212,8 +261,9 @@ publish() {
 
 inform_update() {
     local MESSAGE=$1
-    local INFORM_STATUS=$(curl -s -X POST http://$HOST:3001/inform -F "path=$STREAM_PATH" -F "status=$MESSAGE" -F "action=Update" 1>&1)
-
+    local INFORM_STATUS
+    
+    INFORM_STATUS=$(curl -s -X POST http://$HOST:3001/inform -F "path=$STREAM_PATH" -F "status=$MESSAGE" -F "action=Update" 1>&1)
     if [[ "$INFORM_STATUS" != "0" ]]; then
         echo "Error inform_update code $INFORM_STATUS"
     fi
@@ -221,8 +271,9 @@ inform_update() {
 
 inform_delete() {
     local MESSAGE=$1
-    local INFORM_STATUS=$(curl -s -X POST http://$HOST:3001/inform -F "path=$STREAM_PATH" -F "status=$MESSAGE" -F "action=Delete" 1>&1)
+    local INFORM_STATUS
     
+    INFORM_STATUS=$(curl -s -X POST http://$HOST:3001/inform -F "path=$STREAM_PATH" -F "status=$MESSAGE" -F "action=Delete" 1>&1)
     if [[ "$INFORM_STATUS" != "0" ]]; then
         echo "Error inform_delete code $INFORM_STATUS"
     fi
@@ -237,81 +288,21 @@ close() {
     exit 1
 }
 
+ytCheckStatus() {
+    local STATUS
 
-while true; do
-    inform_update "Checking"
-    echo "Checking status..."
-
-    echo "Use Cookies: $USE_COOKIES"
-
-    if [[ "$USE_COOKIES" == "1" ]]; then
-        COOKIE_ARGS="--js-runtimes bun:$RUNTIME_PATH --cookies "$PW_DIR/config/cookies.txt""
-    else
-        COOKIE_ARGS=""
-    fi
-    
-
-    STATUS=$($YTDLP_PATH $COOKIE_ARGS --no-warnings --print "live_status" "$SOURCE_URL" 2>&1)
-
+    STATUS=$($YTDLP_PATH $(getytdlpCookieArgs) --no-warnings --print "live_status" "$SOURCE_URL" 2>&1)
     if [[ "$STATUS" == "is_live" ]]; then
-
         inform_update "Live Detected"
-
-        # Trap signals and errors 
-        trap close SIGINT SIGTERM EXIT
-
-        # Twitch
-        if [[ "$SOURCE_URL" =~ ^(https?://)?([a-z0-9]+\.)?twitch\.tv ]]; then
-
-            echo "Twitch URL detected."
-            inform_update "Get Channel Name"
-            CH_NAME=$($YTDLP_PATH --no-warnings --print "%(uploader)s" "$SOURCE_URL" | tr '[:upper:]' '[:lower:]' 1>&1)
-
-            inform_update "Get Manifest URL using proxy."
-            echo "Get Manifest URL using proxy."
-            MANIFEST=$($YTDLP_PATH -q --print "url" "https://as.luminous.dev/live/$CH_NAME?allow_source=true&allow_audio_only=true&fast_bread=true" 1>&1)
-            BUFFER="12M"
-            ADD_ARGS="--hls-playlist-reload-time playlist --hls-live-edge 10 --stream-segmented-queue-deadline 6 --stream-segment-timeout 2 --stream-segment-attempts 20"
-            ADD_METADATA="yes"
-        # Youtube
-        elif [[ "$SOURCE_URL" =~ ^(https?://)?([a-z0-9]+\.)?(youtube\.com|youtu\.be) ]]; then
-
-            inform_update "Get Manifest URL"
-            echo "Get Manifest URL."
-            MANIFEST=$($YTDLP_PATH $COOKIE_ARGS -f "b" --no-warnings --print "url" "$SOURCE_URL" 1>&1)
-            BUFFER="12M"
-            ADD_ARGS="--hls-playlist-reload-time playlist --hls-live-edge 10 --stream-segmented-queue-deadline 6 --stream-segment-timeout 2 --stream-segment-attempts 20"
-            ADD_METADATA="yes"
-        # Others
-        else
-            inform_update "Please wait."
-            echo "Use Source URL as a Manifest URL."
-            MANIFEST="$SOURCE_URL"
-            BUFFER="12M"
-            ADD_ARGS="--hls-playlist-reload-time playlist --hls-live-edge 5 --stream-segment-timeout 1 --stream-segment-attempts 50"
-            ADD_METADATA="no"
-        fi
-
-        echo "Starting."
-        inform_update "Starting"
-        publish "$MANIFEST" "$BUFFER" "$ADD_ARGS" "$ADD_METADATA"
-        inform_update "Stopped"
-
-        echo "Ended. Checking once again..."
-    
-    elif [[ "$STATUS" == "upcoming" ]]; then
-        inform_update "Waiting for the LIVE signal"
-        echo "Stream is scheduled. Sleeping 15s..."
-        sleep 15
-        continue
+        return 0
     elif [[ "$STATUS" == "was_live" ]]; then
         inform_update "Error URL"
         echo "Bummer! This link is a VOD!. Exiting..."
-        break
+        return 2
     elif [[ "$STATUS" == "post_live" || "$STATUS" == *"not currently live"* ]]; then
         inform_update "Offline"
         echo "Stream has ended. Exiting...."
-        break
+        return 2
     elif [[ "$STATUS" == *"The page needs to be reloaded"* ]]; then
         inform_update "YouTube caught it. yt-dlp #16212"
         echo "YouTube caught it. Retrying in 5s..."
@@ -319,20 +310,152 @@ while true; do
         echo "Temporarily disabling cookies and retrying..."
         sleep 5
         USE_COOKIES="0"
-        continue
+        return 1
     elif [[ "$STATUS" == "NA" || "$STATUS" == *"not a valid URL"* ]]; then
         inform_update "Unknown URL"
-        echo "Unknown URL."
-        break
+        echo "Unknown URL. Exiting..."
+        return 2
     else
         inform_update "$STATUS"
         echo "Unknown ($STATUS). Retrying in 15s..."
         sleep 15
-        continue
+        return 1
     fi
-    
-    rm -rf "$TMPDIR"
-    inform_update "No Signal"
-    echo "Waiting 5 seconds..."
-    sleep 5
-done
+}
+
+ytGetStreamManifest() {
+    $YTDLP_PATH $(getytdlpCookieArgs) -f "b" --no-warnings --print "url" "$SOURCE_URL" 1>&1
+}
+
+twitchCheckStatus() {
+    local STATUS
+
+    STATUS=$($YTDLP_PATH --no-warnings --print "live_status" "$SOURCE_URL" 2>&1)
+
+    if [[ "$STATUS" == "is_live" ]]; then
+        inform_update "Live Detected"
+        return 0
+    elif [[ "$STATUS" == "was_live" ]]; then
+        inform_update "Error URL"
+        echo "Bummer! This link is a VOD!. Exiting..."
+        return 2
+    elif [[ "$STATUS" == "post_live" || "$STATUS" == *"not currently live"* ]]; then
+        inform_update "Offline"
+        echo "Stream has ended. Exiting...."
+        return 2
+    elif [[ "$STATUS" == "NA" || "$STATUS" == *"not a valid URL"* ]]; then
+        inform_update "Unknown URL"
+        echo "Unknown URL. Exiting..."
+        return 2
+    else
+        inform_update "$STATUS"
+        echo "Unknown ($STATUS). Retrying in 15s..."
+        sleep 15
+        return 1
+    fi
+}
+
+twitchGetStreamManifest() {
+    local CH_NAME
+    local PROXY_HOSTNAME="as.luminous.dev"
+
+    CH_NAME=$($YTDLP_PATH --no-warnings --print "%(uploader)s" "$SOURCE_URL" | tr '[:upper:]' '[:lower:]' 1>&1)
+    $YTDLP_PATH -q --print "url" "https://$PROXY_HOSTNAME/live/$CH_NAME?allow_source=true&allow_audio_only=true&fast_bread=true" 1>&1
+}
+
+othersCheckStatus() {
+    local STATUS
+
+    STATUS=$(curl -s -L -o /dev/null -w "%{http_code}\\n" "$SOURCE_URL")
+
+    if [[ "$STATUS" == "200" ]]; then
+        inform_update "Live Detected"
+        return 0
+    else 
+        inform_update "Offline"
+        return 2
+    fi
+}
+
+main() {
+    while true; do
+        inform_update "Checking"
+        echo "Checking status..."
+
+        echo "Use Cookies: $USE_COOKIES"
+            
+            # Twitch
+            if [[ "$SOURCE_URL" =~ ^(https?://)?([a-z0-9]+\.)?twitch\.tv ]]; then
+                echo "Twitch URL detected."
+
+                twitchCheckStatus
+                local CHECK_STATUS=$?
+                if [[ $CHECK_STATUS == 1 ]]; then
+                    continue
+                elif [[ $CHECK_STATUS == 2 ]]; then
+                    break
+                fi 
+
+                inform_update "Get Manifest URL using proxy."
+                echo "Get Manifest URL using proxy."
+                
+                local MANIFEST=$(twitchGetStreamManifest)
+                local BUFFER="12M"
+                local ADD_ARGS="--hls-playlist-reload-time playlist --hls-live-edge 10 --stream-segmented-queue-deadline 6 --stream-segment-timeout 2 --stream-segment-attempts 20"
+                local ADD_METADATA="yes"
+
+            # Youtube
+            elif [[ "$SOURCE_URL" =~ ^(https?://)?([a-z0-9]+\.)?(youtube\.com|youtu\.be) ]]; then
+                echo "Youtube URL detected."
+
+                ytCheckStatus
+                local CHECK_STATUS=$?
+                if [[ $CHECK_STATUS == 1 ]]; then
+                    continue
+                elif [[ $CHECK_STATUS == 2 ]]; then
+                    break
+                fi 
+
+                inform_update "Get Manifest URL"
+                echo "Get Manifest URL."
+                
+                local MANIFEST=$(ytGetStreamManifest)
+                local BUFFER="12M"
+                local ADD_ARGS="--hls-playlist-reload-time playlist --hls-live-edge 10 --stream-segmented-queue-deadline 6 --stream-segment-timeout 2 --stream-segment-attempts 20"
+                local ADD_METADATA="yes"
+
+            # Others
+            else
+                inform_update "Please wait."
+                echo "Use Source URL as a Manifest URL."
+                
+                othersCheckStatus
+                local CHECK_STATUS=$?
+                if [[ $CHECK_STATUS == 1 ]]; then
+                    continue
+                elif [[ $CHECK_STATUS == 2 ]]; then
+                    break
+                fi 
+
+                local MANIFEST="$SOURCE_URL"
+                local BUFFER="12M"
+                local ADD_ARGS="--hls-playlist-reload-time playlist --hls-live-edge 5 --stream-segment-timeout 1 --stream-segment-attempts 50"
+                local ADD_METADATA="no"
+            fi
+
+        echo "Starting."
+        inform_update "Starting"
+        publish "$MANIFEST" "$BUFFER" "$ADD_ARGS" "$ADD_METADATA"
+        inform_update "Stopped"
+
+        echo "Ended. Checking once again..."
+        
+        rm -rf "$TMPDIR"
+        inform_update "No Signal"
+        echo "Waiting 5 seconds..."
+        sleep 5
+    done
+}
+
+trap close SIGINT SIGTERM EXIT
+main
