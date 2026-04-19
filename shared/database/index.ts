997e4433c62830a732ae5e3d8c7e853b25bbb10b
tripoloskies <rodel.rizalde09@gmail.com>
2026-04-19 20:19:42 +0000

    Use postgres connection pool.

--- shared/database/index.ts
+++ shared/database/index.ts
@@ -1,11 +1,13 @@
+import { Pool } from 'pg';
 import { PrismaClient } from './generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 const globalForPrisma = global as unknown as {
 	prisma: PrismaClient;
 };
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL
+const pool = new Pool({
+	connectionString: Bun.env.DATABASE_URL
 });
+const adapter = new PrismaPg(pool);
 const prisma =
 	globalForPrisma.prisma ||
 	new PrismaClient({
