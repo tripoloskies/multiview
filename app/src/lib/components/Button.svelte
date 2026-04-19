66c475c26de11ed765c7fe2ba54f84346a27d062
tripoloskies <rodel.rizalde09@gmail.com>
2026-04-19 20:38:49 +0000

    Disable svelte/no-navigation-without-resolve lint rule in some files due due to dynamic relative urls that cannot use resolve().

--- app/src/lib/components/Button.svelte
+++ app/src/lib/components/Button.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	/* eslint-disable svelte/no-navigation-without-resolve */
 	let {
 		onclick = defaultClick,
 		type = 'button',
--- app/src/lib/components/PaginationSelector.svelte
+++ app/src/lib/components/PaginationSelector.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	/* eslint-disable svelte/no-navigation-without-resolve */
 	import Button from './Button.svelte';
 
 	const MAX_VISIBLE_PAGES: number = 5;
