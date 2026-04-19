c2e3fef1783349cd7708351426d83886e7647063
tripoloskies <rodel.rizalde09@gmail.com>
2026-04-19 20:32:17 +0000

    Fix unnecessary escape character inside TRIM_LEAD_TRAIL_SLASH_REGEX.

--- shared/utils/regex.ts
+++ shared/utils/regex.ts
@@ -4,4 +4,4 @@ export const TWITCH_URL_REGEX: RegExp =
 export const YT_URL_REGEX: RegExp =
 	/^(https?:\/\/)?([a-z0-9]+\.)?(youtube\.com|youtu\.be)/;
 export const PATH_REGEX: RegExp = /[^A-Za-z0-9/_-]+/g;
-export const TRIM_LEAD_TRAIL_SLASH_REGEX: RegExp = /^[\/\\]+|[\/\\]+$/g;
+export const TRIM_LEAD_TRAIL_SLASH_REGEX: RegExp = /^[\\/]+|[\\/]+$/g;
