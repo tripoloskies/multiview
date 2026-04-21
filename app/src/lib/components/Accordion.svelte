609361b97a03cd82b8454accada993f6fdd2eeb4
tripoloskies <rodel.rizalde09@gmail.com>
2026-04-21 20:21:21 +0000

    "Iconize" accordion component's "Open" and "Close" label.

--- app/src/lib/components/Accordion.svelte
+++ app/src/lib/components/Accordion.svelte
@@ -11,7 +11,39 @@
 	<button onclick={toggle} class="path-accordion-header">
 		{@render header()}
 		<div class="accordion-indicator">
-			<b><u>{isOpen ? 'Close' : 'Open'}</u></b>
+			<b>
+				{#if isOpen}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m4.5 15.75 7.5-7.5 7.5 7.5"
+						/>
+					</svg>
+				{:else}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m19.5 8.25-7.5 7.5-7.5-7.5"
+						/>
+					</svg>
+				{/if}
+			</b>
 		</div>
 	</button>
 	{#if isOpen}
