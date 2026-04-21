163124266c252aeaa6abfdda2ad76bde1122112c
tripoloskies <rodel.rizalde09@gmail.com>
2026-04-21 01:46:12 +0000

    Refactor ambient file by removing unused Platform interface.

--- app/src/app.d.ts
+++ app/src/app.d.ts
@@ -1,14 +1,6 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
-declare global {
-	namespace App {
-		interface Platform {
-			server: Bun.Server;
-			request: Request;
-		}
-		interface Locals {
-			sessionId: string;
-			host: string;
-		}
+namespace App {
+	interface Locals {
+		sessionId: string;
+		host: string;
 	}
 }
